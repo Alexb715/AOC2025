3fdfad8c922b496d268d67a407fd974d5943b092
alex <eab5072@umoncton.ca>
2025-12-09 16:04:26 -0400

    Done Day1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,25 +1,29 @@
 use std::io::{self, BufRead};
+use std::time::Instant; // 1. Add this import
+
 const MAX_SIZE: i32 = 100;
+
 fn left(current_index: i32, steps: i32) -> i32 {
     (current_index - steps).rem_euclid(MAX_SIZE)
 }
-fn right(current_index: i32, steps:i32) -> i32{
+
+fn right(current_index: i32, steps: i32) -> i32 {
     (current_index + steps).rem_euclid(MAX_SIZE)
 }
-fn count_zeros(current: i32, steps: i32, is_right: bool) -> i32{
-    if is_right{
+
+fn count_zeros(current: i32, steps: i32, is_right: bool) -> i32 {
+    if is_right {
         return (current + steps) / MAX_SIZE;
-    }
-    else{
+    } else {
         let dist_to_zero = if current == 0 { MAX_SIZE } else { current };
         if steps < dist_to_zero {
             return 0; 
         } else {
-
             return 1 + (steps - dist_to_zero) / MAX_SIZE;
         }
     }
 }
+
 fn part1(lines: &Vec<String>) {
     let mut my_list = vec![0; MAX_SIZE as usize];
     let mut current_index = 50;
@@ -39,7 +43,8 @@ fn part1(lines: &Vec<String>) {
     my_list.sort();
     println!("Part 1 Answer (Max Value): {}", my_list[(MAX_SIZE - 1) as usize]);
 }
-fn part2(lines: &Vec<String>){
+
+fn part2(lines: &Vec<String>) {
     let mut current_index = 50;
     let mut total_zeros = 0;
 
@@ -58,10 +63,18 @@ fn part2(lines: &Vec<String>){
 
     println!("Part 2 Answer (Total Zeros): {}", total_zeros);
 }
+
 fn main() {
     let stdin = io::stdin();
     let lines: Vec<String> = stdin.lock().lines().map(|l| l.unwrap()).collect();
 
+    let start_p1 = Instant::now();
     part1(&lines);
+    let duration_p1 = start_p1.elapsed();
+    println!("Part 1 took: {:?}", duration_p1);
+
+    let start_p2 = Instant::now();
     part2(&lines);
+    let duration_p2 = start_p2.elapsed();
+    println!("Part 2 took: {:?}", duration_p2);
 }
